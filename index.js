29e94e6268687b47651f7b249b7082b52502fea9
Mehmet <mehmetkusken@gmail.com>
2021-08-03 15:57:01 -0600

    commit tree 168b9e0fc34f7df2899b7922b96c6e45d740e1b6

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "LUKKAAAA is the champion";
+// innerText = document.createTextNode("LUKAAAAA is the champion");
+// newHeader.appendChild(innerText);
+document.body.appendChild(newHeader);
